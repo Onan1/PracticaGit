6395403a9abf3632c68494268c384d94ae6dbe14
Onan1 <pm25001@esfe.agape.edu.sv>
2025-05-28 15:17:37 -0600

    Onan Pineda

---
+++ src/Freelancer.java
@@ -0,0 +1,19 @@
+public abstract class Freelancer {
+    protected String nombre;
+    protected int id;
+    protected int horasTrabajadas;
+
+    public Freelancer(String nombre, int id, int horasTrabajadas) {
+        this.nombre = nombre;
+        this.id = id;
+        this.horasTrabajadas = horasTrabajadas;
+    }
+
+    public abstract double calcularPago();
+
+    public void mostrarInformacion() {
+        System.out.println("ID: " + id);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Horas trabajadas: " + horasTrabajadas);
+    }
+}
