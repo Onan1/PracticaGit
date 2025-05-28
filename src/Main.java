503c70c464a1c3089d8e23ed1f72b7330ec061a6
Vianca2 <pm25004@esfe.agape.edu.sv>
2025-05-28 16:22:12 -0600

    Vianca Pineda

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ArrayList<Freelancer> freelancers = new ArrayList<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        freelancers.add(new ProgramadorFreelance("Carlos", 1, 40, 15.0));
+        freelancers.add(new DiseñadorFreelance("Lucía", 2, 30, 500.0));
+        freelancers.add(new RedactorFreelance("Miguel", 3, 20, 10, 12.0));
+
+        double pagoTotal = 0;
+
+        for (Freelancer f : freelancers) {
+            f.mostrarInformacion();
+            double pago = f.calcularPago();
+            System.out.println("Pago calculado: $" + pago);
+            if (f instanceof Facturable) {
+                ((Facturable) f).generarFactura();
+            }
+            System.out.println("-----------------------------");
+            pagoTotal += pago;
         }
+
+        System.out.println("Pago total a todos los freelancers: $" + pagoTotal);
     }
 }
