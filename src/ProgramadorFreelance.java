208dd0fc6a30fd5539c01b8fe6c80e892b74478f
Onan1 <pm25001@esfe.agape.edu.sv>
2025-05-28 15:25:59 -0600

    Onan Pineda

--- src/ProgramadorFreelance.java
+++ src/ProgramadorFreelance.java
@@ -1,21 +1,20 @@
-public class ProgramadorFreelance {
-    public class ProgramadorFreelance extends Freelancer implements Facturable {
-        private double tarifaPorHora;
+public class ProgramadorFreelance extends Freelancer implements Facturable {
+    private double tarifaPorHora;
 
-        public ProgramadorFreelance(String nombre, int id, int horasTrabajadas, double tarifaPorHora) {
-            super(nombre, id, horasTrabajadas);
-            this.tarifaPorHora = tarifaPorHora;
-        }
+    public ProgramadorFreelance(String nombre, int id, int horasTrabajadas, double tarifaPorHora) {
+        super(nombre, id, horasTrabajadas);
+        this.tarifaPorHora = tarifaPorHora;
+    }
 
-        @Override
-        public double calcularPago() {
-            return horasTrabajadas * tarifaPorHora;
-        }
+    @Override
+    public double calcularPago() {
+        return horasTrabajadas * tarifaPorHora;
+    }
 
-        @Override
-        public void generarFactura() {
-            System.out.println("Factura del Programador:");
-            System.out.println("Total a pagar: $" + calcularPago());
-        }
+    @Override
+    public void generarFactura() {
+        System.out.println("Factura del Programador:");
+        System.out.println("Total a pagar: $" + calcularPago());
     }
 }
+
